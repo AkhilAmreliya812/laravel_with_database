c68de683d0241f0e1c29c85162af1a1ee1735dfb
Akhil Amreliya <Akhil.Amreliya@netclues.local>
2025-08-05 13:04:35 +0530

    Multiple data inserted using another way with seeders

--- database/seeders/WorkerSeeder.php
+++ database/seeders/WorkerSeeder.php
@@ -14,14 +14,25 @@ class WorkerSeeder extends Seeder
     public function run(): void
     {
 
+    // ************** Multiple data insert ***********************
     //    DB::table('workers')->insert([
     //     ['name' => 'Akhil Amreliya','email' => 'akhil@gmail.com','city' => 'Ahmedabad'],
     //     ['name' => 'Harsh Patel','email' => 'harsh@gmail.com','city' => 'Vejalpur'],
     //     ['name' => 'Bhupat Vatukiya','email' => 'bhupat@gmail.com','city' => 'Bavla']
     // ]);
 
-    DB::table('workers')->insert([
-        'name' => 'Bhupat Vatukiya','email' => 'bhupat@gmail.com','city' => 'Bavla'
-    ]);
+    // ************** Single data insert ***********************
+    // DB::table('workers')->insert([
+    //     'name' => 'Bhupat Vatukiya','email' => 'bhupat@gmail.com','city' => 'Bavla'
+    // ]);
+
+    // ************** Multipl data insert another way ***********************
+    $workers = [
+        ['name' => 'Akhil Amreliya','email' => 'akhil@gmail.com','city' => 'Ahmedabad'],
+        ['name' => 'Harsh Patel','email' => 'harsh@gmail.com','city' => 'Vejalpur'],
+        ['name' => 'Bhupat Vatukiya','email' => 'bhupat@gmail.com','city' => 'Bavla']
+    ];
+
+    DB::table('workers')->insert($workers);
     }
 }
